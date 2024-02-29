65dc715dd9d46bdd8a2ce14d2b36651a30737d70
Ritesh Taru <Ritesh Taru@MSI>
2024-02-29 09:27:44 +0530

    Final Commit

---
+++ session_05/src/session_05/Test.java
@@ -0,0 +1,20 @@
+package session_05;
+
+public class Test {
+	public static void main(String args[])
+	{
+		int[] arr=new int[5];
+		System.out.println(arr[0]);
+		
+		for(int i=0;i<arr.length;i++)
+		{
+			arr[i]=i+1;
+		}
+		System.out.println("Array Element are as follows:");
+		for(int a:arr)
+		{
+			System.out.println(a);
+		}
+	}
+
+}
